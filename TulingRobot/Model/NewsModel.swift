8789111c54f0b1669aeba280fcabc29d70c77ad5
15011213159 <1134532311@qq.com>
2018-06-24 20:14:34 +0800

    NewsModel

---
+++ TulingRobot/Model/NewsModel.swift
@@ -0,0 +1,17 @@
+//
+//  NewsModel.swift
+//  TulingRobot
+//
+//  Created by wuxi on 2018/6/24.
+//  Copyright © 2018年 demo. All rights reserved.
+//
+
+import UIKit
+
+let appKey = "23942860"
+let appSK = "2d087550f1296820ca9c6005ab9b6ac0"
+let appCode = "2840f55e174d49d78f65fdb866a26894"
+
+class NewsModel: NSObject {
+
+}
